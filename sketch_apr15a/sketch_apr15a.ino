92774d5474cf406d95991905e6106cdf562aa89f
Jaffar3984 <j.raza3835@gmail.com>
2019-04-25 21:34:30 -0400

    Create sketch_apr15a.ino

---
+++ sketch_apr15a/sketch_apr15a.ino
@@ -0,0 +1,16 @@
+#include <Servo.h>
+
+Servo myServo;
+
+
+void setup() {
+  myServo.attach(9);
+  myServo.write(100);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
